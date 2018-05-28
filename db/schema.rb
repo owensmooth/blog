eb99dc2d30eaa8019f600d452e869250be20250c
Owen Smith <ozz1649@gmail.com>
2018-05-28 20:43:39 +0100

    Added tests for adding, editing and viewing a blog post

--- db/schema.rb
+++ db/schema.rb
@@ -31,14 +31,5 @@ ActiveRecord::Schema.define(version: 2018_05_03_121912) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.boolean "published"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "comments", "articles"
 end
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -31,5 +31,22 @@ describe 'my special blog' do
       expect(current_path).to eq("/articles/#{Article.last.id}")
       expect(page).to have_content('Flappy')
     end
+
+    it 'edits a blog post' do
+      create :article, title: 'beans'
+      visit_auth "/admin/articles/#{Article.last.id}"
+
+      expect(page).to have_content('beans')
+
+      visit_auth "/admin/articles/#{Article.last.id}/edit"
+
+      fill_in 'Title', with: 'Edited Post'
+      fill_in 'Text', with: 'Just edited'
+
+      click_button 'Update Article'
+
+      expect(current_path).to eq("/articles/#{Article.last.id}")
+      expect(page).to have_content('Edited Post')
+    end
   end
 end
