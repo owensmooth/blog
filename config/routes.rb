71ef0c6ba4e3c308912e55c8fbe7bccec362e392
Owen Smith <ozz1649@gmail.com>
2018-06-08 16:53:29 +0100

    added styling to articles and main layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'articles/index'
 
   resources :articles, only: [:index, :show] do
     resources :comments
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
     resources :articles, :comments
   end
 
-  root 'welcome#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
