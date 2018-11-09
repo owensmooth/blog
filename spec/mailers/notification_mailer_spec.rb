2a0c1a72f053656a8361c625cfc6fc3c93af00b9
Owen Smith <ozz1649@gmail.com>
2018-11-09 11:07:39 +0000

    Added tests for email service class, added multiple emails to notification factory to be tested

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,10 +1,20 @@
 require "rails_helper"
 
 RSpec.describe NotificationMailer do
-  describe "Sending an email when new article created", type: :mailer do
+  describe "Sending an email when new article created", type: :class do
     let!(:notification) { create(:notification) }
     let(:article) { create(:article) }
-    let(:mail) { NotificationMailer.notification_email(article) }
+    let(:mail) { NotificationMailer.notification_email() }
+
+    it "should be valid" do
+      expect(mail.from).to have_content("owen.smith@shiftcommerce.com")
+      expect(mail.to).to have_content("test@test.com")
+    end
+  end
+
+    describe "Sending an email when new user signed up", type: :class do
+    let!(:notification) { create(:notification) }
+    let(:mail) { NotificationMailer.signup_email(notification: notification) }
 
     it "should be valid" do
       expect(mail.from).to have_content("owen.smith@shiftcommerce.com")
