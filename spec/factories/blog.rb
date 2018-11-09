b64e7758e596ef6cc6acda943ba47c3336ab0cfe
Owen Smith <ozz1649@gmail.com>
2018-11-09 11:34:06 +0000

    Changed single quotes to double

--- spec/factories/blog.rb
+++ spec/factories/blog.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
-    name {'test'}
-    email {'test@test.com'}
-    password {'password'}
-    password_confirmation {'password'}
+    name {"test"}
+    email {"test@test.com"}
+    password {"password"}
+    password_confirmation {"password"}
   end
 
   factory :notification do
-    email {'test@test.com' 'test123@test.com'}
+    email {"test@test.com" "test123@test.com"}
   end
 end
