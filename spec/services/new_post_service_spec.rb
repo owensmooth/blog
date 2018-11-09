45d14d9dfc83fd8e2fdfaed85fbfb3158f2ce704
Owen Smith <ozz1649@gmail.com>
2018-11-09 11:50:06 +0000

    Added untracked folders for spec/services

---
+++ spec/services/new_post_service_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe NewPostService do
+  describe "Sending an email to all users when new article created", type: :mailer do
+    let(:article) { create(:article) }
+    let!(:notification) { create(:notification) }
+    let(:mail) { NewPostService.call() }
+
+    it "should be valid and be sent to all users" do
+      expect(mail.from).to have_content("owen.smith@shiftcommerce.com")
+      expect(mail.to).to have_content("test@test.com")
+      expect(mail.to).to have_content("test123@test.com")
+      expect(mail.subject).to match("New blog post!!!")
+      expect(mail.body.encoded).to match("Hi, a new article has been posted")
+    end
+  end
+end
---
+++ spec/services/sign_up_service_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe SignUpService do
+  describe "Sending an email when signed up for notifications", type: :mailer do
+    let!(:notification) { create(:notification) }
+    let(:mail) {SignUpService.call(notification: notification) }
+
+    it "should be valid" do
+      expect(mail.from).to have_content("owen.smith@shiftcommerce.com")
+      expect(mail.to).to have_content("test123@test.com")
+      expect(mail.subject).to match("Welcome")
+      expect(mail.body.encoded).to match("Hi, you are now signed up for email notifications")
+    end
+  end
+end
